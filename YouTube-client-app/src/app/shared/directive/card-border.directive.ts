1b79df84ab5540abbc9b08ace0c7246f7d3b64ad
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-11-08 00:26:01 +0300

    refactor: delete ? from the template

--- YouTube-client-app/src/app/shared/directive/card-border.directive.ts
+++ YouTube-client-app/src/app/shared/directive/card-border.directive.ts
@@ -7,24 +7,22 @@ import { BorderColors } from '../enums/enums';
 })
 
 export class CardBorderDirective implements OnInit {
-  @Input('appCardBorder') publishedAt: string | undefined;
+  @Input('appCardBorder') publishedAt = '';
 
   constructor(private renderer: Renderer2, private element: ElementRef) { }
 
   ngOnInit(): void {
     const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
-    if (this.publishedAt !== undefined) {
-      const days = Math.floor(new Date().getTime() - new Date(this.publishedAt).getTime())
-        / oneDayInMilliseconds;
-      if (days < 7) {
-        this.renderer.addClass(this.element.nativeElement, BorderColors.BLUE);
-      } else if (days >= 7 && days < 31) {
-        this.renderer.addClass(this.element.nativeElement, BorderColors.GREEN);
-      } else if (days >= 31 && days <= 180) {
-        this.renderer.addClass(this.element.nativeElement, BorderColors.YELLOW);
-      } else if (days >= 180) {
-        this.renderer.addClass(this.element.nativeElement, BorderColors.RED);
-      }
+    const days = Math.floor(new Date().getTime() - new Date(this.publishedAt).getTime())
+      / oneDayInMilliseconds;
+    if (days < 7) {
+      this.renderer.addClass(this.element.nativeElement, BorderColors.BLUE);
+    } else if (days >= 7 && days < 31) {
+      this.renderer.addClass(this.element.nativeElement, BorderColors.GREEN);
+    } else if (days >= 31 && days <= 180) {
+      this.renderer.addClass(this.element.nativeElement, BorderColors.YELLOW);
+    } else if (days >= 180) {
+      this.renderer.addClass(this.element.nativeElement, BorderColors.RED);
     }
   }
 }
