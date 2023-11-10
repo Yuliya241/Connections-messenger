e10dccff0810d27eb2a5fb12cf613f9adced47a9
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-11-10 10:37:45 +0300

    feat: "Tags" sub-form is implemented

--- YouTube-client-app/src/app/admin/pages/admin-page/admin-page.component.ts
+++ YouTube-client-app/src/app/admin/pages/admin-page/admin-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { dateValidator } from '../../date-validator';
 
@@ -11,15 +11,54 @@ import { dateValidator } from '../../date-validator';
 export class AdminPageComponent {
   isSubmitted = false;
 
+  count = 1;
+
   formAdmin = this.fb.group({
     title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
     description: ['', Validators.maxLength(255)],
     image: ['', Validators.required],
     linkToVideo: ['', Validators.required],
     creationDate: ['', [Validators.required, dateValidator()]],
+    tags: this.fb.array([this.createTagsFormGroup()]),
   });
 
   constructor(private fb: FormBuilder) { }
+
+  createTagsFormGroup(): FormGroup {
+    return this.fb.group({
+      tag: ['', Validators.required],
+    });
+  }
+
+  get tagsControls() {
+    return (this.formAdmin.get('tags') as FormArray).controls;
+  }
+
+  addTags() {
+    this.count += 1;
+    if (this.count <= 5) {
+      const tags = this.formAdmin.get('tags');
+      if (tags instanceof FormArray) {
+        tags.push(this.createTagsFormGroup());
+      }
+    }
+  }
+
+  removeTags() {
+    const tags = this.formAdmin.get('tags');
+    if (tags instanceof FormArray) {
+      while (tags.length !== 1) {
+        tags.removeAt(0);
+        this.count = 1;
+      }
+    }
+  }
+
+  resetAll() {
+    this.formAdmin.reset();
+    this.removeTags();
+    this.isSubmitted = false;
+  }
 
   get title() {
     return this.formAdmin.controls.title;
